0af8aa95cdf4983d834e65917690154afde1db2f
dongxin <dongxin2019@gmail.com>
2021-01-08 09:53:56 +0800

    topic 189; run 92ms 82.3%; ram 39.6mb 38.50%;

--- topic/189.js
+++ topic/189.js
@@ -4,10 +4,7 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-    while (k > 0) {
-        nums.unshift(nums.pop())
-        k-=1
-    }
+    nums.splice(0, 0, ...nums.splice(nums.length - k, k))
     return nums
 };
 let a = rotate([1,2,3,4,5,6,7], 3)
